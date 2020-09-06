680e6a381543d523f28d2d41e223cf26e0e3b553
Pantelis <6103339+Sadulisten@users.noreply.github.com>
2020-09-06 07:46:12 +0200

    Recovered from catastrophe?

--- TestInject/MyLibrary.cs
+++ TestInject/MyLibrary.cs
@@ -1,102 +1,116 @@
 ﻿using System;
-using System.Collections;
 using System.Diagnostics;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Windows.Forms;
 using static TestInject.Memory;
-using static TestInject.CSGO.Enums;
+using static TestInject.DebugConsole;
+using static TestInject.Memory.Enums;
+using static TestInject.AssaultCube.Delegates;
 
+using static TestInject.AssaultCube;
+
+using static TestInject.Aura_Kingdom.Structures;
 
 namespace TestInject
 {
 	public class MyLibrary
 	{
-		[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-		public delegate int GetPlayerInCrosshair();
+		public static Detour.HookObj<GetPlayerEntityInCrosshairDelegate> getPlayerInCrossHook;
 
-		[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-		public delegate void TraceLine(float fX, float fY, float fZ,
-			float tX, float tY, float tZ, IntPtr localplayer, bool bCheckPlayers, bool bSkipTags);
-		
+		public static Detour.HookObj<glEndDelegate> glEndHook;
 
-		public static Detours.HookEx<GetPlayerInCrosshair> MyHk;
-
-		public static Detours.BasicHook<GetPlayerInCrosshair> MyHook;
+		public const bool IMPLEMENT_GL_HOOKS = true;
 
 		[DllExport("DllMain", CallingConvention.Cdecl)]
-		public static void EntryPoint()
+		public static unsafe void EntryPoint()
 		{
-			//PatchETW(true);
 			UpdateProcessInformation();
 			AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
 			
-			if (!DebugConsole.InitiateDebugConsole())
+			if (!InitiateDebugConsole())
 				MessageBox.Show("Failed initiating the debugging console, please restart the program as admin!",
 					"Debugging Console Exception",
 					MessageBoxButtons.OK,
 					MessageBoxIcon.Error);
 
-			/*
-			IntPtr mod = Modules.GetModuleBaseAddress("SDL.dll");
-			uint oSDLSwapBuffers = (uint)PInvoke.GetProcAddress(mod, "SDL_GL_SwapBuffers");
-			if (oSDLSwapBuffers != 0)
-			{
-				Console.WriteLine($"SDLSwapBuffers: 0x{oSDLSwapBuffers:X8}");
-				SwapBuffersHK = new Detours.Hk<SDLSwapBuffers>(oSDLSwapBuffers, HkSDLSwapBuffers, 5, true);
-				SwapBuffersHK.Install();
-			}
-			else
-				Console.WriteLine("Failed SDLSwapBuffers");
-			*/
+			//new Thread(SetUpHooks).Start();
+			//new Thread(() => new Overlay("AssaultCube").ShowDialog()).Start();
 
-			//MyHk = new Detours.HookEx<GetPlayerInCrosshair>(
-			//	GetPlayerInCrosshairFunction,
-			//	del,
+			SelectedTarget* current = Aura_Kingdom.Pointers.SelectedTargetInformation;
+			uint address = (uint)current;
+			Console.WriteLine($"0x{address:X8} Name: {current->Name()}");
+
+
+			Console.ReadLine();
+		}
+
+		public static void SetUpHooks()
+		{
+			if (IMPLEMENT_GL_HOOKS)
+			{
+				IntPtr openGlModule = Modules.GetModuleBaseAddress("opengl32.dll");
+				if (openGlModule == IntPtr.Zero)
+					Log($"Cannot find base address of 'opengl32.dll'", LogType.Error);
+				else
+				{
+					#region glEnd
+					IntPtr glEnd = PInvoke.GetProcAddress(openGlModule, "glEnd");
+					if (glEnd != IntPtr.Zero)
+					{
+						IntPtr glVertex3f = PInvoke.GetProcAddress(openGlModule, "glVertex3f");
+						OpenGL.glVertex3f = Marshal.GetDelegateForFunctionPointer<glVertex3fDelegate>(glVertex3f);
+
+						IntPtr glColor3f = PInvoke.GetProcAddress(openGlModule, "glColor3f");
+						OpenGL.glColor3f = Marshal.GetDelegateForFunctionPointer<glColor3fDelegate>(glColor3f);
+
+						glEndHook = new Detour.HookObj<glEndDelegate>(glEnd, glEnd_hk, 6);
+						if (!glEndHook.Install())
+							Log($"[glEnd] Failed applying jmp hook at 0x{glEnd.ToInt32():X8}", LogType.Error);
+						else
+							Log($"[glEnd] Successfully applied jmp hook at 0x{glEnd.ToInt32():X8}");
+					}
+					else
+						Log($"Failed getting base address of function 'glEnd' from module 'opengl32.dll'", LogType.Error);
+
+					#endregion
+				}
+			}
+
+			#region GetPlayerEntityInCrosshair
+			//getPlayerInCrossHook = new Detour.HookObj<GetPlayerEntityInCrosshairDelegate>(new IntPtr(0x004607C0),
+			//	GetPlayerEntityInCrosshair_hk,
 			//	6);
 
-			//MyHk.Install();
+			// Can call .Install for getPlayerInCrossHook if you want
+			// Lets not implement the hook, and instead just keep it like this and call the function
+			// with getPlayerInCrossHook.UnmodifiedOriginalFunction() to get the player in our crosshair
+			#endregion
 
-			GetPlayerInCrosshair hookDelegate = HkGetPlayerInCrosshair;
-			IntPtr GetPlayerInCrosshairFunction = new IntPtr(0x004607C0); // GetPlayerInCrosshair
-
-			MyHook = new Detours.BasicHook<GetPlayerInCrosshair>(GetPlayerInCrosshairFunction, hookDelegate,
-				6);
-			MyHook.Install();
-
-			new Thread(() => new Overlay("Counter-Strike: Global Offensive").ShowDialog()).Start();
+			Console.WriteLine($"Thread 'SetUpHooks()' finished execution!");
 		}
 
-		[MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-		public static unsafe int HkGetPlayerInCrosshair()
-		{
-			Console.WriteLine($"[{DateTime.Now.ToLongTimeString()}] Hallo we're in managed code in a unmanaged executable :33");
-			
-			return MyHook.Original();
-		}
-
-		public static unsafe void MyCallbackMethod(IntPtr registerPtr)
-		{
-			if (registerPtr == IntPtr.Zero)
-			{
-				Console.WriteLine("MyCallbackMethod - Failed");
-				return;
-			}
-
-			Structures.Registers* reg = (Structures.Registers*)registerPtr;
-			Console.WriteLine(reg->PrintRegisters());
-
-			// We know that the pointer in ESI points to the value of our current ammo
-			if ((int*) reg->ESI != null)
-			{
-				*(int*)reg->ESI = new Random().Next(1, 1337);
-				Console.WriteLine($"Current Ammo: {*(int*)reg->ESI}");
-			}
-
-			return;
-		}
 		
+		public static void glEnd_hk()
+		{
+			Console.WriteLine($"[{DateTime.Now.ToLongTimeString()}] Inside glEnd_hk");
+
+			// Draw here
+
+			//OpenGL.glColor3f(125, 125, 125);
+			//OpenGL.glVertex3f(100, 100, 0.0f);
+
+			glEndHook.UnmodifiedOriginalFunction();
+		}
+
+		public static int GetPlayerEntityInCrosshair_hk()
+		{
+			// No real use for this hook, was just playing around
+			// can call
+
+			return getPlayerInCrossHook.UnmodifiedOriginalFunction();
+		}
+
 		static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
 		{
 			HelperMethods.PrintExceptionData(e?.ExceptionObject, true);
@@ -108,24 +122,5 @@ namespace TestInject
 			if (Debugger.IsAttached)
 				Debugger.Break();
 		}
-
-		public static void PatchETW(bool verbose = false)
-		{
-			var ntdll = Modules.GetModuleBaseAddress("ntdll");
-			if (ntdll == IntPtr.Zero)
-				ntdll = PInvoke.LoadLibrary("ntdll.dll");
-
-			if (verbose) Console.WriteLine($"ntdll: 0x{ntdll.ToInt32():X8}");
-
-			var etwEventSend = PInvoke.GetProcAddress(ntdll, "EtwEventWrite");
-			if (verbose) Console.WriteLine($"EtwEventWrite: 0x{etwEventSend.ToInt32():X8}");
-			if (etwEventSend != IntPtr.Zero)
-			{
-				Protection.SetPageProtection(etwEventSend, 3, Enums.MemoryProtection.ExecuteReadWrite, out var old);
-				Writer.WriteBytes(etwEventSend, new byte[] { 0xc2, 0x14, 0x00 });
-				Protection.SetPageProtection(etwEventSend, 3, old, out _);
-				if (verbose) Console.WriteLine("EtwEventWrite Patching Done!");
-			}
-		}
-	}
+    }
 }
